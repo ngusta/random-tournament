950892cefdf5bce97752888ad779c64a197061aa
Gustafsson <Niklas.Gustafsson@assaabloy.com>
2024-12-31 03:12:35 +0100

    Upgraded to React 18.3.1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {Route, BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Presentation from './Presentation';
 import Leaderboard from './Leaderboard';
 import * as serviceWorker from './serviceWorker';
+import { createRoot } from 'react-dom/client';
 
 const routing = (
     <Router>
@@ -17,7 +17,10 @@ const routing = (
     </Router>
 );
 
-ReactDOM.render(routing, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(routing);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
