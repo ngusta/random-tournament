fff2c92ce4501294afda960b390b144768c6f965
Gustafsson <Niklas.Gustafsson@assaabloy.com>
2025-01-05 01:30:48 +0100

    Specified react-router-dom version so that the newest version (7.1.1) that doesn't seem to work with the rest of my stuff got pushed. Chose react-router-dom 6.x that required some minor changes.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import {Route, BrowserRouter as Router} from 'react-router-dom';
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Presentation from './Presentation';
 import Leaderboard from './Leaderboard';
 import PlayerView from './PlayerView';
 import * as serviceWorker from './serviceWorker';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 
 const routing = (
     <Router>
-        <div>
-            <Route exact path="/" component={App}/>
-            <Route path="/presentation" component={Presentation}/>
-            <Route path="/leaderboard" component={Leaderboard}/>
-            <Route path="/playerview/:tournamentId" component={PlayerView}/>
-        </div>
+        <Routes>
+            <Route exact path="/" element={<App/>}/>
+            <Route path="/presentation" element={<Presentation/>}/>
+            <Route path="/leaderboard" element={<Leaderboard/>}/>
+            <Route path="/playerview/:tournamentId" element={<PlayerView/>}/>
+        </Routes>
     </Router>
 );
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(routing);
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
